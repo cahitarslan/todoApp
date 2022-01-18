53219d5df10fa46d9d15f19eaf683a5fbf39eb81
Cahit Arslan <cahitarslan.business@gmail.com>
2022-01-18 15:33:42 +0300

    lesson 3: Model Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todoApp';
+  user = 'Cahit';
+  items = [
+    { description: 'Kahvaltı', action: 'No' },
+    { description: 'Sinema', action: 'No' },
+    { description: 'Spor', action: 'No' },
+    { description: 'Ders Çalışma', action: 'No' },
+  ];
 }
